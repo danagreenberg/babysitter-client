9be89b1b94620d955d9a0ba8882c58f07840c357
danagreenberg <danagreenberg2001@gmail.com>
2026-06-27 17:48:00 +0300

    chore: split server into its own repository

--- server/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error('❌ Server error:', err.message);
-  res.status(err.status || 500).json({
-    success: false,
-    error: err.message || 'שגיאת שרת פנימית'
-  });
-};
-
-module.exports = errorHandler;
--- server/routes/reviews.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const { getReviewsBySitter, createReview } = require('../controllers/reviewsController');
-const { protect } = require('../middleware/auth');
-
-router.get('/sitter/:sitterId', getReviewsBySitter);
-router.post('/',                protect, createReview);
-
-module.exports = router;
